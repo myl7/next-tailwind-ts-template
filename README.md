93a81ef1a4004a1d2a701b9bb97b6bcc69ea7e72
myl7 <myl@myl.moe>
2022-03-06 17:58:32 +0800

    Update page root components and add favicon

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Modern Next.js + TailwindCSS + Typescript template
 Copyright (c) 2022 myl7
 
 SPDX-License-Identifier: Apache-2.0 OR MIT
+
+About icons in `public`: Icons made by [Freepik](https://www.freepik.com) from [www.flaticon.com](https://www.flaticon.com/)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,11 @@
+/**
+ * Copyright (c) myl7 2022
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
+
 import '../styles/globals.css'
+import '../styles/reset.css'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
